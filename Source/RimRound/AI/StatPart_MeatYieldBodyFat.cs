f7bd5329d0c9ec0434be43e03d4a8b2b90d68d53
Niwatori401 <niwatori401@aaathats3as.com>
2022-01-03 21:49:52 -0800

    Add new textures by @ArtOfFire1 (not yet implemented in-game) Add feature: pawns now yield differing amounts of meat based on current weight

---
+++ Source/RimRound/AI/StatPart_MeatYieldBodyFat.cs
@@ -0,0 +1,55 @@
+﻿using RimRound.Utilities;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace RimRound.AI
+{
+    public class StatPart_MeatYieldBodyFat : StatPart
+    {
+        public override string ExplanationPart(StatRequest req)
+        {
+            if (req.Thing is Pawn p && p.RaceProps.Humanlike) 
+            {
+                float mult = GetMeatMultByWeight(req);
+
+                return "StatPart_MeatYieldBodyFatExplanation".Translate(mult.ToString("F2")) + ": x" + mult.ToStringPercent();
+            }
+
+            return null;
+        }
+
+        public override void TransformValue(StatRequest req, ref float val)
+        {
+            if (req.HasThing)
+            {
+                if (req.Thing is Pawn pawn && pawn.RaceProps.Humanlike)
+                {
+                    val *= GetMeatMultByWeight(req);
+                    return;
+                }
+            }
+            return;
+        }
+
+        float GetMeatMultByWeight(StatRequest req) 
+        {
+            if (req.HasThing)
+            {
+                if (req.Thing is Pawn pawn)
+                {
+                    if (pawn.WeightHediff() is Hediff weightHediff)
+                    {
+                        return ((Utilities.HediffUtility.SeverityToKilosWithBaseWeight(weightHediff.Severity) + 225) * 0.5f) / 140;
+                    }
+                }
+            }
+
+            return 1;
+        }
+    }
+}
