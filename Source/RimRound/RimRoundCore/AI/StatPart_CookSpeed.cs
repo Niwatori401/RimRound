761c6e18c5c3c442cbfeff2bfeb818ed8e0e9ec5
Niwatori401 <cjackson@firemail.cc>
2022-09-23 18:44:49 +0900

    Implement 'Culinary Connoisseur' perk Implement 'Demonic Devourment/Breakneck Buffet' perks

---
+++ Source/RimRound/RimRoundCore/AI/StatPart_CookSpeed.cs
@@ -0,0 +1,36 @@
+﻿using RimRound.Comps;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace RimRound.AI
+{
+    internal class StatPart_CookSpeed : StatPart
+    {
+        public override string ExplanationPart(StatRequest req)
+        {
+            return "Culinary Connoisseur";
+        }
+
+        public override void TransformValue(StatRequest req, ref float val)
+        {
+            if (req.Thing is Pawn pawn)
+            {
+                if (pawn.RaceProps.Humanlike)
+                {
+                    FullnessAndDietStats_ThingComp comp = pawn.TryGetComp<FullnessAndDietStats_ThingComp>();
+                    if (comp is null)
+                        return;
+
+                    int culinaryConniseurLevel = comp.perkLevels.PerkToLevels["RR_Culinary_Connisseur_Title"];
+
+                    val += culinaryConniseurLevel * 0.1f;
+                }
+            }
+        }
+    }
+}
---
+++ Source/RimRound/RimRoundCore/AI/StatPart_EatingSpeed.cs
@@ -0,0 +1,37 @@
+﻿using RimRound.Comps;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace RimRound.AI
+{
+    internal class StatPart_EatingSpeed : StatPart
+    {
+        public override string ExplanationPart(StatRequest req)
+        {
+            return "Demonic devourment / Breakneck buffet";
+        }
+
+        public override void TransformValue(StatRequest req, ref float val)
+        {
+            if (req.Thing is Pawn pawn)
+            {
+                if (pawn.RaceProps.Humanlike)
+                {
+                    FullnessAndDietStats_ThingComp comp = pawn.TryGetComp<FullnessAndDietStats_ThingComp>();
+                    if (comp is null)
+                        return;
+
+                    int demonicDevourmentLevel = comp.perkLevels.PerkToLevels["RR_Demonic_Devourment_Title"];
+                    int breakneckbuffetLevel = comp.perkLevels.PerkToLevels["RR_Breakneck_Buffet_Title"];
+
+                    val += demonicDevourmentLevel * 0.1f + breakneckbuffetLevel * 0.25f;
+                }
+            }
+        }
+    }
+}
