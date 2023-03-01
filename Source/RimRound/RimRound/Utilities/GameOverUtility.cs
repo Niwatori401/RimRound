03b65fe132dbc4372290aa3ead6560f6f8965701
Niwatori401 <->
2023-03-01 21:51:44 +0900

    Update credits Clarify which perks are needed for 'Ascension'

--- Source/RimRound/RimRound/Utilities/GameOverUtility.cs
+++ Source/RimRound/RimRound/Utilities/GameOverUtility.cs
@@ -27,16 +27,10 @@ namespace RimRound.Utilities
                 }
             }
 
-            ShowCredits(MakeEndCredits(AscensionIntro, AscensionEnding, launchedPawns.ToString(), "", listOfPawns), SongDefOf.EndCreditsSong, true, 3f);
+            ShowCredits(MakeEndCredits("RimRound_Credits_Ascension_Intro".Translate(), "RimRound_Credits_Ascension_Body".Translate(), launchedPawns.ToString(), "", listOfPawns), SongDefOf.EndCreditsSong, true, 3f);
 
         }
 
-        static string AscensionIntro = "You've done it. Your ceaseless quest for gluttony has allowed you to expand to a size so colossal, that an alien ship was able to spot you from orbit. Intrigued, they came to investigate, and were fascinated with the...results. They decided to assist you, not in leaving the planet, but in making you a test subject.\n";
-
-        static string AscensionEnding = "Their technologies and culinary preparations were far beyond comprehension, and the subject had little choice in this new undertaking. In due time, their current size was dwarfed, and their exponential growth continued unimpeded. The world beneath them shrank, until they took its place, becoming the land itself. With only the vast expanse of space to challenge their scale, their conquest did not end there, on the contrary, it was merely beginning.\n\nFattened beyond even mortal understanding, the subject became so immense that even many archotech societies took notice, and, eventually, cultivated a galactic empire using their eternally expanding form. Some say they are there to this very day, in a state of perpetual bliss, their inescapable fate quite literally written in the stars.\n\nYou have chosen your destiny.";
-
-        static string RR_CreditsSectionTitle = "RimRound Credits";
-        static string RR_CreditsSection = "\n\nProgramming: Niwatori401\nArt: BambooAle";
 
         private static string MakeEndCredits(string intro, string ending, string escapees, string colonistsEscapeeTKey, IList<Pawn> escaped = null) 
         {
@@ -65,8 +59,8 @@ namespace RimRound.Utilities
             FieldInfo credsFI = typeof(Screen_Credits).GetField("creds", BindingFlags.Instance | BindingFlags.NonPublic);
             List<CreditsEntry> credits = (List<CreditsEntry>)credsFI.GetValue(screen_Credits);
             credits.Add(new CreditRecord_Space(200f));
-            credits.Add(new CreditRecord_Text(RR_CreditsSectionTitle, TextAnchor.MiddleCenter));
-            credits.Add(new CreditRecord_Text(RR_CreditsSection, TextAnchor.UpperLeft));
+            credits.Add(new CreditRecord_Title("RimRound_Credits_Title".Translate()));
+            credits.Add(new CreditRecord_Text("RimRound_Credits_Description".Translate(), TextAnchor.UpperLeft));
 
             credsFI.SetValue(screen_Credits, credits);
 
