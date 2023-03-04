c31789bcb7b1e00ea26a355099f9c99f1e2f6c7f
Niwatori401 <->
2023-03-04 22:04:19 +0900

    Fix bug where burkas and other 'shell' clothing items would display poorly on RR body types

---
+++ Source/RimRound/RimRound/Patch/RimWorldPatches/PawnRenderer/PawnRenderer_ShellFullyCoversHead_AlterShellClothingForRRBodies.cs
@@ -0,0 +1,41 @@
+﻿using HarmonyLib;
+using RimRound.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace RimRound.Patch
+{
+    /// <summary>
+    /// This patch enabled hair to be drawn if a pawn is wearing a shell clothing item that normally covers their head and they have an RR body.
+    /// </summary>
+    [HarmonyPatch(typeof(PawnRenderer))]
+    [HarmonyPatch("ShellFullyCoversHead")]
+    public class PawnRenderer_ShellFullyCoversHead_AlterShellClothingForRRBodies
+    {
+        static FieldInfo pawnFieldInfo = typeof(PawnRenderer).GetField("pawn", BindingFlags.Instance | BindingFlags.NonPublic);
+
+        public static void Postfix(ref bool __result, PawnRenderer __instance) 
+        {
+            if (!__result)
+                return;
+
+            Pawn pawn = (Pawn)pawnFieldInfo.GetValue(__instance);
+
+            if (pawn is null)
+            {
+                Log.Error($"Pawn was null in {nameof(PawnRenderer_ShellFullyCoversHead_AlterShellClothingForRRBodies.Postfix)}");
+                return;
+            }
+
+            if (BodyTypeUtility.HasCustomBody(pawn))
+                __result = false;
+
+            return;
+        }
+    }
+}
