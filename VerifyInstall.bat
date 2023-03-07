75c5dfd21b4825f5ee028d115750de5674a01848
Niwatori401 <->
2023-03-08 07:11:43 +0900

    Update verify install scripts

--- VerifyInstall.bat
+++ VerifyInstall.bat
@@ -1,17 +1,19 @@
 @ECHO OFF
 setlocal EnableDelayedExpansion
+ECHO Hashing files... (This can take a few minutes)
 FOR /R %%L IN (*) DO (
 	SET FILENAME=%%L
 	IF x"!FILENAME:.git\\=!"==x"!FILENAME!" (
 		IF x"!FILENAME:output.md5=!"==x"!FILENAME!" (
 			IF x"!FILENAME:INTEGRITY_HASH=!"==x"!FILENAME!" (
-				certutil -hashfile "!filepath!" MD5 >> output.md5
+				IF x"!FILENAME:RESULT_HASH=!"==x"!FILENAME!" (
+					IF x"!FILENAME:.bat=!"==x"!FILENAME!" (
+						certutil -hashfile "!filepath!" MD5 >> output.md5
+					)
+				)
 			)
 		)
 	)
 )
-COPY output.md5 ..\
-CD ..
-RMDIR /Q /S SHA_MEASURE
 certutil -hashfile ".\output.md5" MD5 > RESULT_HASH
 DEL output.md5
--- make_md5.bat
+++ make_md5.bat
@@ -3,16 +3,20 @@ setlocal EnableDelayedExpansion
 MKDIR SHA_MEASURE
 git clone ./ SHA_MEASURE/ 
 CD SHA_MEASURE/
+ECHO Hashing files... (This can take a few minutes)
 FOR /R %%L IN (*) DO (
 	SET FILENAME=%%L
 	IF x"!FILENAME:.git\\=!"==x"!FILENAME!" (
 		IF x"!FILENAME:output.md5=!"==x"!FILENAME!" (
 			IF x"!FILENAME:INTEGRITY_HASH=!"==x"!FILENAME!" (
-				certutil -hashfile "!filepath!" MD5 >> output.md5
+				IF x"!FILENAME:.bat=!"==x"!FILENAME!" (
+					certutil -hashfile "!filepath!" MD5 >> output.md5
+				)
 			)
 		)
 	)
 )
+ECHO Cleaning up
 COPY output.md5 ..\
 CD ..
 RMDIR /Q /S SHA_MEASURE
