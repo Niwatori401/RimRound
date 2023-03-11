eb16c5bc1d2c31133f97ad222cc79d12ba6a6456
Niwatori401 <->
2023-03-11 20:10:07 +0900

    Fix immobilized pawns who are in beds appearing offset horizontally

--- Source/RimRound/RimRound/Patch/RimWorldPatches/GraphicalPatches/PortraitsCache_RenderPortrait_DisablePawnPortraitRotationForPawnsInBed.cs
+++ Source/RimRound/RimRound/Patch/RimWorldPatches/GraphicalPatches/PortraitsCache_RenderPortrait_DisablePawnPortraitRotationForPawnsInBed.cs
@@ -27,38 +27,64 @@ namespace RimRound.Patch
         }
         
 
-        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) 
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator) 
         {
             List<CodeInstruction> codeInstructions = new List<CodeInstruction>(instructions);
             List<CodeInstruction> newInstructions = new List<CodeInstruction>();
 
-            MethodInfo getAngleMI = typeof(PortraitsCache_RenderPortrait_DisablePawnPortraitRotationForPawnsInBed)
-                .GetMethod(nameof(GetAngle), BindingFlags.NonPublic | BindingFlags.Static);
+            MethodInfo shouldSkipOffsetMI = typeof(PortraitsCache_RenderPortrait_DisablePawnPortraitRotationForPawnsInBed)
+                .GetMethod(nameof(ShouldSkipOffsets), BindingFlags.NonPublic | BindingFlags.Static);
 
-            if (getAngleMI is null)
-                Log.Error("shouldBeSidewaysMI was null in PortraitsCache_RenderPortrait patch!");
+            MethodInfo pawnCacheRenderGetMI = typeof(Find).GetProperty(nameof(Find.PawnCacheRenderer), BindingFlags.Static | BindingFlags.Public).GetGetMethod(true);
 
+            if (pawnCacheRenderGetMI is null)
+            {
+                Log.Error($"pawnCacheRenderGetMI was null in {nameof(PortraitsCache_RenderPortrait_DisablePawnPortraitRotationForPawnsInBed)}");
+            }
 
+            bool foundInsertionPoint = false;
 
             for (int i = 0; i < codeInstructions.Count; ++i) 
             {
                 if (codeInstructions[i].opcode == OpCodes.Ldc_R4 && (float)codeInstructions[i].operand == 85) 
                 {
-                    codeInstructions[i] = new CodeInstruction(OpCodes.Ldarg_1);
-                    codeInstructions.Insert(i + 1, new CodeInstruction(OpCodes.Call, getAngleMI));
+                    foundInsertionPoint = true;
+                    bool foundLabel = false;
+                    Label label = generator.DefineLabel();
+                    for (int j = i; j < codeInstructions.Count; ++j)
+                    {
+                        if (codeInstructions[j].Calls(pawnCacheRenderGetMI))
+                        {
+                            codeInstructions[j].labels.Add(label);
+                            foundLabel = true;
+                        }
+                    }
+
+                    if (!foundLabel)
+                    {
+                        Log.Error($"Failed to find label for transpiler in {nameof(PortraitsCache_RenderPortrait_DisablePawnPortraitRotationForPawnsInBed)}");
+                    }
+
+                    newInstructions.Add(new CodeInstruction(OpCodes.Ldarg_1)); // Branch out of block conditionally
+                    newInstructions.Add(new CodeInstruction(OpCodes.Call, shouldSkipOffsetMI));
+                    newInstructions.Add(new CodeInstruction(OpCodes.Brtrue, label));
+
+                    codeInstructions.InsertRange(i, newInstructions);
+
                     break;
                 }
             }
 
+            if (!foundInsertionPoint)
+                Log.Error($"Failed to find suitable insert location in patch {nameof(PortraitsCache_RenderPortrait_DisablePawnPortraitRotationForPawnsInBed.Transpiler)}.");
+
             return codeInstructions;
         }
 
-        static float GetAngle(Pawn pawn) 
+        // we already know the pawn is downed or dead
+        static bool ShouldSkipOffsets(Pawn pawn)
         {
-            if (pawn.Dead || (pawn.Downed && !pawn.InBed()))
-                return 85f;
-
-            return 0f;
+            return pawn.InBed();
         }
     }
 }
