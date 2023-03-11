1c099a6670be6522ca0267f941ded5f2f253df53
Niwatori401 <->
2023-03-11 21:01:52 +0900

    Replace broken patch that previously prevented HAR from generating RR bodies erroneously

---
+++ Source/RimRound/RimRound/Patch/HumanoidAlienRacesPatches/HarmonyPatches_CheckBodyType_ChangeRRToThin.cs
@@ -0,0 +1,26 @@
+﻿using AlienRace;
+using HarmonyLib;
+using RimRound.Utilities;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace RimRound.Patch
+{
+    [HarmonyPatch(typeof(HarmonyPatches))]
+    [HarmonyPatch(nameof(HarmonyPatches.CheckBodyType))]
+    internal class HarmonyPatches_CheckBodyType_ChangeRRToThin
+    {
+        public static void Postfix(ref BodyTypeDef __result) 
+        {
+            if (BodyTypeUtility.IsRRBody(__result))
+                __result = RimWorld.BodyTypeDefOf.Thin;
+
+            return;
+        }
+    }
+}
