d2142271c7c7436bbb422bf86c5a7c7660d29f1b
Niwatori401 <->
2023-03-18 09:00:43 +0900

    Fix RR bodies being bugged for genes that used body graphics

---
+++ Source/RimRound/RimRound/Patch/RimWorldPatches/FurDef_GetFurBodyGraphicPath_ReturnTransparentForRR.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using RimRound.Utilities;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace RimRound.Patch
+{
+    [HarmonyPatch(typeof(FurDef))]
+    [HarmonyPatch(nameof(FurDef.GetFurBodyGraphicPath))]
+    public class FurDef_GetFurBodyGraphicPath_ReturnTransparentForRR
+    {
+        public static void Postfix(ref string __result, Pawn pawn) 
+        {
+            if (BodyTypeUtility.HasCustomBody(pawn))
+                __result = "BlankTexture";
+
+            return;
+        }
+    }
+}
