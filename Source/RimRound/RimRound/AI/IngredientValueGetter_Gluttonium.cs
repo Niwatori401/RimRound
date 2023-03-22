2adbe24a47bbdd6f7e00e17fd9b1d87577933503
Niwatori401 <->
2023-03-22 15:55:59 +0900

    Fix UltraTech bench not being usable Increase work amount for making gluttonium meals beyond trivial value

---
+++ Source/RimRound/RimRound/AI/IngredientValueGetter_Gluttonium.cs
@@ -0,0 +1,29 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace RimRound.AI
+{
+    internal class IngredientValueGetter_Gluttonium : IngredientValueGetter
+    {
+        public override string BillRequirementsDescription(RecipeDef r, IngredientCount ing)
+        {
+            return ing.GetBaseCount() + "x " + "BillNutrition".Translate() + " (" + ing.filter.Summary + ")";
+        }
+
+        public override float ValuePerUnitOf(ThingDef t)
+        {
+            if (t == Defs.ThingDefOf.RR_Gluttonium)
+                return 1f;
+
+            if (!t.IsNutritionGivingIngestible)
+                return 0f;
+
+            return t.GetStatValueAbstract(StatDefOf.Nutrition, null);
+        }
+    }
+}
