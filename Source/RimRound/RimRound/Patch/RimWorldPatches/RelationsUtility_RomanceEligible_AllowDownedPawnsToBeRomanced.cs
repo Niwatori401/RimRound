5f362a8832c6f9eb5683d919934108aa7c8b2846
Niwatori401 <->
2023-03-28 11:49:34 +0900

    Downed pawns that are capable of talking can now be interacted with socially

---
+++ Source/RimRound/RimRound/Patch/RimWorldPatches/RelationsUtility_RomanceEligible_AllowDownedPawnsToBeRomanced.cs
@@ -0,0 +1,56 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace RimRound.Patch
+{
+    [HarmonyPatch(typeof(RelationsUtility))]
+    [HarmonyPatch(nameof(RelationsUtility.RomanceEligible))]
+    public class RelationsUtility_RomanceEligible_AllowDownedPawnsToBeRomanced
+    {
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) 
+        {
+            List<CodeInstruction> codeInstructions = new List<CodeInstruction>(instructions);
+
+            bool foundHook = false;
+
+            MethodInfo getDownedMI = typeof(RelationsUtility).GetProperty(nameof(Pawn.Downed)).GetGetMethod(true);
+            MethodInfo replacementCheckMI =
+                typeof(RelationsUtility_RomanceEligible_AllowDownedPawnsToBeRomanced)
+                .GetMethod(nameof(RelationsUtility_RomanceEligible_AllowDownedPawnsToBeRomanced.ReplacementMethod), BindingFlags.Static | BindingFlags.NonPublic);
+
+            if (getDownedMI is null)
+                Log.Error("Failed to get MethodInfo for Downed()");
+
+            if (replacementCheckMI is null)
+                Log.Error("Failed to get MethodInfo for Downed()");
+
+            for (int i = 0; i < codeInstructions.Count; i++)
+            {
+                if (!codeInstructions[i].Calls(getDownedMI))
+                    continue;
+
+                foundHook = true;
+                codeInstructions[i] = new CodeInstruction(OpCodes.Call, replacementCheckMI);
+            }       
+
+            if (!foundHook)
+                Log.Error($"Failed to find insertion point in {nameof(RelationsUtility_RomanceEligible_AllowDownedPawnsToBeRomanced)}");
+
+            return codeInstructions;
+        }
+
+        //Return true if should not be elligible
+        private static bool ReplacementMethod(Pawn pawn)
+        {
+            return pawn.Downed && !pawn.InBed();
+        }
+    }
+}
