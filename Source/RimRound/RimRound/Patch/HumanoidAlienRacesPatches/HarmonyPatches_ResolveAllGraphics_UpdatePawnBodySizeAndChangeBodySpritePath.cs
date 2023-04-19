ae83c3bbc1e3ce4c78c87eeea0a60fc08b2d9ddd
Niwatori401 <->
2023-04-19 19:28:27 +0900

    Fix HAR patch

--- Source/RimRound/RimRound/Patch/HumanoidAlienRacesPatches/HarmonyPatches_ResolveAllGraphics_UpdatePawnBodySizeAndChangeBodySpritePath.cs
+++ Source/RimRound/RimRound/Patch/HumanoidAlienRacesPatches/HarmonyPatches_ResolveAllGraphics_UpdatePawnBodySizeAndChangeBodySpritePath.cs
@@ -79,9 +79,9 @@ namespace RimRound.Patch
         private static bool ChangeDrawSize(List<CodeInstruction> codeInstructions)
         {
             FieldInfo customDrawSizeLifeStageAlienFI = typeof(LifeStageAgeAlien).GetField(nameof(LifeStageAgeAlien.customDrawSize));
-            FieldInfo customDrawSizeLifeStageAlienFemaleFI = typeof(LifeStageAgeAlien).GetField(nameof(LifeStageAgeAlien.customFemaleDrawSize));
+            FieldInfo customDrawSizeAlienCompFI = typeof(AlienPartGenerator.AlienComp).GetField(nameof(AlienPartGenerator.AlienComp.customDrawSize));
 
-            if (customDrawSizeLifeStageAlienFemaleFI is null || customDrawSizeLifeStageAlienFI is null)
+            if (customDrawSizeAlienCompFI is null || customDrawSizeLifeStageAlienFI is null)
                 return false;
 
             bool success1 = false;
@@ -89,7 +89,7 @@ namespace RimRound.Patch
 
             for (int i = 0; i < codeInstructions.Count; i++)
             {
-                if (!success1 && codeInstructions[i].operand is FieldInfo fi && fi == customDrawSizeLifeStageAlienFI)
+                if (success2 && !success1 && codeInstructions[i].operand is FieldInfo fi && fi == customDrawSizeLifeStageAlienFI)
                 {
                     //Portrait draw size
                     codeInstructions.Remove(codeInstructions[i+7]); // Remove call to LifeStageAgeAlien.customDrawSize
@@ -109,22 +109,15 @@ namespace RimRound.Patch
                     success1 = true;
                 }
 
-                else if (!success2 && codeInstructions[i].operand is FieldInfo fi2 && fi2 == customDrawSizeLifeStageAlienFemaleFI)
-                {                    
-                    //Portrait draw size
-                    codeInstructions.Remove(codeInstructions[i+7]); // Remove call to LifeStageAgeAlien.customDrawSizeFemale
-                    codeInstructions.Remove(codeInstructions[i+7]);
+                else if (!success2 && codeInstructions[i].opcode == OpCodes.Stfld && codeInstructions[i].operand is FieldInfo fi2 && fi2 == customDrawSizeAlienCompFI)
+                {
+                    codeInstructions.Insert(i + 24, new CodeInstruction(OpCodes.Call, ReplacementMethodInfo));
+                    codeInstructions.Insert(i + 24, new CodeInstruction(OpCodes.Ldloc_2)); // AlienComp
+                    codeInstructions.Insert(i + 24, new CodeInstruction(OpCodes.Pop)); // AlienLifeStage
 
-                    codeInstructions.Insert(i + 7, new CodeInstruction(OpCodes.Call, ReplacementMethodInfo)); // Replace with call to new function and duplicate argument needed for later that also needs consumed by the function
-                    codeInstructions.Insert(i + 7, new CodeInstruction(OpCodes.Dup));
-
-
-                    //Regular (female) drawsize
-                    codeInstructions.Remove(codeInstructions[i - 1]);
-                    codeInstructions.Remove(codeInstructions[i - 1]);
-
-                    codeInstructions.Insert(i - 1, new CodeInstruction(OpCodes.Call, ReplacementMethodInfo));
-                    codeInstructions.Insert(i - 1, new CodeInstruction(OpCodes.Dup));
+                    codeInstructions.Insert(i, new CodeInstruction(OpCodes.Call, ReplacementMethodInfo));
+                    codeInstructions.Insert(i, new CodeInstruction(OpCodes.Ldloc_2)); // AlienComp
+                    codeInstructions.Insert(i, new CodeInstruction(OpCodes.Pop)); // AlienLifeStage
 
                     success2 = true;
                 }
