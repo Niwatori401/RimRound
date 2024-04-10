9448ceb156799504d5adf97aaef839f40d54bdc4
Niwatori401 <->
2024-04-09 21:51:10 -0700

    Re-enable corpse clothing cache invlidation patch Fix pawn redraw function

--- Source/RimRound/RimRound/Patch/RimWorldPatches/GameDataSaveLoader_LoadGame_WipeCaches.cs
+++ Source/RimRound/RimRound/Patch/RimWorldPatches/GameDataSaveLoader_LoadGame_WipeCaches.cs
@@ -9,20 +9,20 @@ using Verse;
 
 namespace RimRound.Patch
 {
-    ///// <summary>
-    ///// Wipes all static caches on load.
-    ///// </summary>
-    //[HarmonyPatch(typeof(GameDataSaveLoader))]
-    //[HarmonyPatch(nameof(GameDataSaveLoader.LoadGame), new Type[] { typeof (string) })]
-    //public class GameDataSaveLoader_LoadGame_WipeCaches
-    //{
-    //    public static void Postfix() 
-    //    {
-    //        BodyTypeUtility.InvalidateCorpseCache();
-    //        RacialBodyTypeInfoUtility.InvalidateCaches();
-    //        PawnRenderer_GetBodyPos_HideBlankets.InvalidateCaches();
-    //        Corpse_CurRotDrawMode_Get_ChangeDessicatedCorpseBodyTypeToNonRR.InvalidateCache();
-    //        ApparelGraphicRecordGetter_TryGetGraphicApparel_UseTransparentImagesForBadTex.InvalidateCache();
-    //    }
-    //}
+    /// <summary>
+    /// Wipes all static caches on load.
+    /// </summary>
+    [HarmonyPatch(typeof(GameDataSaveLoader))]
+    [HarmonyPatch(nameof(GameDataSaveLoader.LoadGame), new Type[] { typeof(string) })]
+    public class GameDataSaveLoader_LoadGame_WipeCaches
+    {
+        public static void Postfix()
+        {
+            BodyTypeUtility.InvalidateCorpseCache();
+            RacialBodyTypeInfoUtility.InvalidateCaches();
+            PawnRenderer_GetBodyPos_HideBlankets.InvalidateCaches();
+            Corpse_CurRotDrawMode_Get_ChangeDessicatedCorpseBodyTypeToNonRR.InvalidateCache();
+            ApparelGraphicRecordGetter_TryGetGraphicApparel_UseTransparentImagesForBadTex.InvalidateCache();
+        }
+    }
 }
