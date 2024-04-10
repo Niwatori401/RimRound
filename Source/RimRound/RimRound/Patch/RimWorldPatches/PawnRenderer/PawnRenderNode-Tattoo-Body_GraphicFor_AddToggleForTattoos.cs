fbc06047624a34fa39bf3c71c8893b5bdac0349b
Niwatori401 <->
2024-04-10 00:24:00 -0700

    Restore tattoo hiding for custom body patch

---
+++ Source/RimRound/RimRound/Patch/RimWorldPatches/PawnRenderer/PawnRenderNode-Tattoo-Body_GraphicFor_AddToggleForTattoos.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using RimRound.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace RimRound.Patch
+{
+    [HarmonyPatch(typeof(PawnRenderNode_Tattoo_Body))]
+    [HarmonyPatch(nameof(PawnRenderNode_Tattoo_Body.GraphicFor))]
+    public class PawnRenderNode_Tattoo_Body_GraphicFor_AddToggleForTattoos
+    {
+        public static void Postfix(ref Graphic __result, Pawn pawn) {
+            if (!GlobalSettings.showBodyTatoosForCustomSprites && BodyTypeUtility.HasCustomBody(pawn)) {
+                __result = null;
+            }
+        }
+    }
+}
